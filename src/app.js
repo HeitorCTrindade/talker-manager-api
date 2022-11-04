60b70a5ea1f88f6e5d80b417d1688816a675c301
HeitorCTrindade <heitor-trindade@hotmail.com>
2022-11-04 18:05:34 -0300

    construindo o servidor

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-// gogo Iniciando o projeto
+// gogo Iniciando o projetoclear
+
+const express = require('express');
+
+const app = express();
+
+app.get('/talker', (req, res) => res.status(200).json({ message: 'olá Mundo!' }));
+
+module.exports = app;
